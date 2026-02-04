296c74775461ceb6e8ee7dda1d11ddde795f890d
GustavoTiburcio <gustavotiburcio23@gmail.com>
2026-02-04 17:55:57 -0300

    fix: export AuthProvider via context index

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,24 +2,24 @@ import { createContext, useState } from 'react';
 import { Toast } from '../components/Toast';
 
 interface IAuthContextValue {
-  showToast: ({ message, type }: { message: string, type: 'success' | 'error' | 'info' }) => void;
+  showToast: ({ message, type }: { message: string; type: 'success' | 'error' | 'info' }) => void;
 }
 
 export const AuthContext = createContext({} as IAuthContextValue);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const [toast, setToast] = useState({ show: false, message: '', type: 'success' as 'success' | 'error' | 'info' });
+  const [toast, setToast] = useState({
+    show: false,
+    message: '',
+    type: 'success' as 'success' | 'error' | 'info'
+  });
 
-  function showToast({ message, type }: { message: string, type: 'success' | 'error' | 'info' }) {
+  function showToast({ message, type }: { message: string; type: 'success' | 'error' | 'info' }) {
     setToast({ show: true, message, type });
   }
 
   return (
-    <AuthContext.Provider
-      value={{
-        showToast
-      }}
-    >
+    <AuthContext.Provider value={{ showToast }}>
       <Toast
         message={toast.message}
         type={toast.type}
